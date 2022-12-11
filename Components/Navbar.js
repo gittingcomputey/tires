e9f5a7b74618f7eb550541fddeba6712e8fc9bfd
gittingcomputey <gettingcomputey@protonmail.com>
2022-12-11 01:18:20 -0800

    Front-page design done

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,8 +1,11 @@
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import styles from "../styles/Home.module.scss";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
+
   const [isMobile, setIsMobile] = useState(null);
   const setNavType = () => {
     window.innerWidth > 600 ? setIsMobile(false) : setIsMobile(true);
@@ -13,21 +16,31 @@ const Navbar = () => {
 
   return (
     <>
-      {!isMobile ? (
-        <nav className={styles.naviContainer}>
-          <Link className={styles.naviEach} href="/home">
-            Home
-          </Link>
-          <Link className={styles.naviEach} href="/contact">
-            Contact
-          </Link>
-          <Link className={styles.naviEach} href="/services">
-            Services
-          </Link>
-        </nav>
-      ) : (
-        <nav className={styles.naviBurgerContainer}>hamburger</nav>
-      )}
+      <div className={styles.naviWrapper}>
+        {!isMobile ? (
+          <nav className={styles.naviContainer}>
+            {router.pathname !== "/" ? (
+              <Link className={styles.naviEach} href="/home">
+                Home
+              </Link>
+            ) : (
+              ""
+            )}
+
+            <Link className={styles.naviEach} href="/services">
+              Services
+            </Link>
+            <Link className={styles.naviEach} href="/contact">
+              Contact
+            </Link>
+            <Link className={styles.naviEach} href="/services">
+              Specials
+            </Link>
+          </nav>
+        ) : (
+          <nav className={styles.naviBurgerContainer}>hamburger</nav>
+        )}
+      </div>
     </>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import styles from "../styles/Home.module.scss";
 import Layout from "../Components/Layout";
 
-import { GiTireTracks } from "react-icons/gi";
+import { GiTireTracks, GiTinker, GiTireIron, GiCarWheel } from "react-icons/gi";
+import { BiBandAid } from "react-icons/bi";
 
 export default function Home() {
   return (
@@ -22,32 +23,80 @@ export default function Home() {
           <div className={styles.homeTeaserContainer}>
             <div className={styles.homeTeaserBox}>
               <p className={styles.homeTeaserText}>
-                CLC Tires has been servicing The San Fernando valley since DATE.
-                We are family owned and operated, fast and reliable, and offer a
-                wide variety of new and used tires. Our professional and
-                experienced team members are ready to help you with all of your
-                tire needs.
+                <span className={styles.homeTeaserName}>CLC Tires</span> has
+                been servicing The San Fernando valley since DATE. We are family
+                owned and operated, fast and reliable, and offer a wide variety
+                of new and used tires. Our professional and experienced team
+                members are ready to help you with all of your tire needs.
               </p>
-              <p className={styles.homeTeaserData}>
-                15412 Nordhoff St North Hills, CA 91343
-              </p>
-              <p className={styles.homeTeaserData}>818-830-5189</p>
+              <div className={styles.homeTeaserAddyBox}>
+                <p className={styles.homeTeaserData}>
+                  15412 Nordhoff St <br></br> North Hills, CA 91343
+                </p>
+                <p className={styles.homeTeaserData}>
+                  PHONE: <br></br>818-830-5189
+                </p>
+              </div>
             </div>
           </div>
 
+          {/* services section */}
           <div className={styles.homeServicesContainer}>
             <div className={styles.homeServicesBox}>
               <div className={styles.homeServicesHeadlineBox}>
-                <GiTireTracks className={styles.homeTireTracks} />
-                <h2 className={styles.homeServicesHeadline}>Services</h2>
-                <GiTireTracks className={styles.homeTireTracks} />
+                <h2 className={styles.homeServicesHeadline}>--Services--</h2>
+              </div>
+              <div className={styles.homeServicesContentBox}>
+                <ul className={styles.homeServicesContentUL}>
+                  <li>
+                    <GiCarWheel className={styles.homeServicesContentIcon} />
+                  </li>
+                  <li className={styles.homeServicesContentLI}>New tires</li>
+                  <li>
+                    <GiCarWheel className={styles.homeServicesContentIcon} />
+                  </li>
+                  <li className={styles.homeServicesContentLI}>Used tires</li>
+                  <li>
+                    <BiBandAid className={styles.homeServicesContentIcon} />
+                  </li>
+                  <li className={styles.homeServicesContentLI}>
+                    Puncture Patching
+                  </li>
+                  <li>
+                    <GiTireIron className={styles.homeServicesContentIcon} />
+                  </li>
+                  <li className={styles.homeServicesContentLI}>
+                    Alignment and Balancing
+                  </li>
+                </ul>
               </div>
             </div>
             <div className={styles.homeServicesBox}>
               <div className={styles.homeServicesHeadlineBox}>
-                <GiTireTracks className={styles.homeTireTracks} />
-                <h2 className={styles.homeServicesHeadline}>Reviews</h2>
-                <GiTireTracks className={styles.homeTireTracks} />
+                <h2 className={styles.homeServicesHeadline}>
+                  --Appreciation--
+                </h2>
+              </div>
+              <div className={styles.homeServicesContentBox}>
+                <p className={styles.homeServicesContentReview}>
+                  I had a tire pop on the freeway and needed a replacement
+                  quick! I knew CLC was the place to go to for a used tire that
+                  could get me back on the road at a reasonable price. I was
+                  back on the road in a quick minute! -JZ
+                </p>
+                <p className={styles.homeServicesContentReview}>
+                  Sergio and the rest of the team never lag to help a girl out
+                  in need when she pops a tire or has another nail in the benz.
+                  -CU
+                </p>
+                <p className={styles.homeServicesContentReview}>
+                  Really friendly and super helpful when I said I didn't know
+                  how to buy brake pads. -ARG
+                </p>
+                <p className={styles.homeServicesContentReview}>
+                  By chance on the corner of Nordhoff and Sepulveda I made a u
+                  turn and I found an awesome place. -CDA
+                </p>
               </div>
             </div>
           </div>
